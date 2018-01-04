c221d0fc7be180cde9580efe92398f8e450a7c2e
manonair <manonair20@gmail.com>
2018-01-03 19:16:41 -0500

    updated for DiscoveryClient config

--- user-mgmnt-service/src/main/java/com/mt/ums/UserManagementApplication.java
+++ user-mgmnt-service/src/main/java/com/mt/ums/UserManagementApplication.java
@@ -2,7 +2,10 @@ package com.mt.ums;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author m1015830
@@ -13,6 +16,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EntityScan("com.mt.ums.domin.entity")
+@EnableJpaRepositories("com.mt.ums.repository")
+@EnableDiscoveryClient
 public class UserManagementApplication 
 {
     public static void main( String[] args )
